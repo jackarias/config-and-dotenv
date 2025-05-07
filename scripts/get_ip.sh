4e86a7cf73b5f718e4a13aa72df754fb55c47e4b
jackarias <javier.arias88@gmail.com>
2025-05-07 17:58:11 -0300

    Updated some configs. Added config file for ghostty

--- scripts/get_ip.sh
+++ scripts/get_ip.sh
@@ -1,7 +1,7 @@
 function get_ip () {
   INTERFACE=$(ip -4 route | grep default | head -n1 | awk '{print $5}')
   if [ ! -z $INTERFACE ]; then
-    echo "$(ip -4 -o addr show scope global wifi0 | awk '{print $4'} | sed 's/\/.*//')"
+    echo "$(ip -4 -o addr show scope global $INTERFACE | awk '{print $4}' | sed 's/\/.*//')"
   else
     echo -n "||"
   fi
